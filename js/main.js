719866440e84d6a3dcf2a3742088ca1486e2dc3f
yovchenko <yovchenko.w@gmail.com>
2017-10-05 00:24:35 +0300

    changed navbar style

--- js/main.js
+++ js/main.js
@@ -3,20 +3,36 @@ let offsetButton = (function () {
 let scrollOffset = document.getElementById('contentWrap').offsetTop;
 return scrollOffset;
 });
+var scrollElm = document.scrollingElement;
 window.onresize = offsetButton;
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-  
-    if (document.body.scrollTop > offsetButton() || document.documentElement.scrollTop > offsetButton()) {
+    if (scrollElm.scrollTop > offsetButton()) {
         document.getElementById('scrollTop').style.display = "block";
-    } else {
+    }
+    else {
         document.getElementById('scrollTop').style.display = "none";
     }
+    if (scrollElm.scrollTop >= 105) {
+        $('.navbar').addClass('scrolled-nav');
+    }
+    else {
+        $('.navbar').removeClass('scrolled-nav');
+    } 
 }
-
 $('#scrollTop').on('click', function(event){
     event.stopPropagation();
     $('html,body').stop().animate({ scrollTop: 0 }, 'slow');
     return false;
 });
+document.getElementsByClassName('navbar-toggle')[0].onclick = function () {
+    var scrolledNav = document.getElementsByClassName('navbar')[0];
+    if (this.getAttribute("aria-expanded") === 'false' || this.getAttribute("aria-expanded") === null && !scrolledNav.classList.contains("activeBar")) {
+        scrolledNav.classList.add("activeBar");
+        scrolledNav.style.height = 'auto';
+    } 
+    else {
+        scrolledNav.classList.remove("activeBar");
+    }
+}
 });
