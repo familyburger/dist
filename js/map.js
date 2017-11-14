c77065cd9ea2bd8bc8ce9b68ede8d3f692fe9fd0
yovchenko <yovchenko.w@gmail.com>
2017-11-15 00:52:03 +0200

    added header margin

--- js/map.js
+++ js/map.js
@@ -222,9 +222,27 @@ $(function () {
             }
             ]
         }
-    
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+        var markerImage = 'svg/marker.svg';
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: markerImage
+        });
+        var contentString = '<div class="info-window">' +
+            '<h3>FamilyBurger</h3>' +
+            '<div class="info-content">' +
+            '<h4>Ласкаво просимо!</h4>' +
+            '</div>' +
+            '</div>';
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 400
+        });
 
-
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
     }
 
     google.maps.event.addDomListener(window, 'load', initMap);
