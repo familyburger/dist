4972dfeef13f00253917426a5ef5aa1986bc57c6
yovchenko <yovchenko.w@gmail.com>
2017-11-14 21:22:19 +0200

    added scroll preventDefault

--- js/main.js
+++ js/main.js
@@ -1,42 +1,40 @@
 $(document).ready(function () {
-let offsetButton = (function () {
-let scrollOffset = document.getElementById('contentWrap').offsetTop;
-return scrollOffset;
-});
-let scrollElm = document.scrollingElement;
-window.onresize = offsetButton;
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    console.log(offsetButton() + ' ' + scrollElm.scrollTop)
-    if (scrollElm.scrollTop > offsetButton()) {
-        document.getElementById('scrollTop').style.display = "block";
+    let offsetButton = (function () {
+        let scrollOffset = document.getElementById('contentWrap').offsetTop;
+        return scrollOffset;
+    });
+    let scrollElm = document.scrollingElement;
+    window.onresize = offsetButton;
+    window.onscroll = function () {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        if (scrollElm.scrollTop > offsetButton()) {
+            document.getElementById('scrollTop').style.display = "block";
+        } else {
+            document.getElementById('scrollTop').style.display = "none";
+        }
+        if (scrollElm.scrollTop >= 105) {
+            $('.navbar').addClass('scrolled-nav');
+        } else {
+            $('.navbar').removeClass('scrolled-nav');
+        }
     }
-    else {
-        document.getElementById('scrollTop').style.display = "none";
+    $('#scrollTop').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').stop().animate({
+            scrollTop: 0
+        }, 'slow');
+        return false;
+    });
+    document.getElementsByClassName('navbar-toggle')[0].onclick = function () {
+        let scrolledNav = document.getElementsByClassName('navbar')[0];
+        if (this.getAttribute("aria-expanded") === 'false' || this.getAttribute("aria-expanded") === null && !scrolledNav.classList.contains("activeBar")) {
+            scrolledNav.classList.add("activeBar");
+            scrolledNav.style.height = 'auto';
+        } else {
+            scrolledNav.classList.remove("activeBar");
+        }
     }
-    if (scrollElm.scrollTop >= 105) {
-        $('.navbar').addClass('scrolled-nav');
-    }
-    else {
-        $('.navbar').removeClass('scrolled-nav');
-    } 
-}
-jQuery.easing.quart = function (x, t, b, c, d) {
-    return -c * ((t=t/d-1)*t*t*t - 1) + b;
-};
-$('#scrollTop').on('click', function(event){
-    event.stopPropagation();
-                    jQuery('html,body').animate({ scrollTop: 0 }, 1200, 'quart');
-                    return false;
-});
-document.getElementsByClassName('navbar-toggle')[0].onclick = function () {
-    let scrolledNav = document.getElementsByClassName('navbar')[0];
-    if (this.getAttribute("aria-expanded") === 'false' || this.getAttribute("aria-expanded") === null && !scrolledNav.classList.contains("activeBar")) {
-        scrolledNav.classList.add("activeBar");
-        scrolledNav.style.height = 'auto';
-    } 
-    else {
-        scrolledNav.classList.remove("activeBar");
-    }
-}
 });
