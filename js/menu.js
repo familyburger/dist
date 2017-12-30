a9f57f4159b4d3365660eb35e4e0f208725cc944
yovchenko <yovchenko.w@gmail.com>
2017-12-30 13:02:34 +0200

    changed img placeholder

--- js/menu.js
+++ js/menu.js
@@ -60,7 +60,15 @@ $(document).ready(function () {
       }
     }
   })();
+  window.addEventListener('load', function () {
+    const allimages = document.getElementsByTagName('img');
+    for (var i = 0; i < allimages.length; i++) {
+      if (allimages[i].getAttribute('data-src')) {
+        allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+      }
+    }
+  }, false)
   $('.notify-badge').arctext({
     radius: 300
   });
-}, true);
+});
